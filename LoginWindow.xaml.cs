b386e263057c679a800faa2240c1b126c18d2c26
Dounia Benabdelâli <dounia.benabdelali@student.ehb.be>
2023-11-15 21:04:05 +0100

    Login-register

--- LoginWindow.xaml.cs
+++ LoginWindow.xaml.cs
@@ -19,20 +19,52 @@ namespace Examen.Net
     /// </summary>
     public partial class LoginWindow : Window
     {
+        private readonly Data.DatabaseContext dbContext;
+
+        // Public property om de ingelogde gebruiker door te geven
+        public Models.User LoggedInUser { get; private set; }
+
         public LoginWindow()
         {
             InitializeComponent();
+            dbContext = new Data.DatabaseContext();
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            // Voeg inloglogica toe
-            MessageBox.Show("Inloggen...");
-        }
+            try
+            {
+                // Haal de gebruiker op basis van de ingevoerde inloggegevens
+                var enteredUsername = UsernameTextBox.Text;
+                var enteredPassword = PasswordBox.Password;
 
+                var loggedInUser = dbContext.Users.FirstOrDefault(user => user.Username == enteredUsername && user.Password == enteredPassword);
+
+                if (loggedInUser != null)
+                {
+                    // Inloggen was succesvol
+                    LoggedInUser = loggedInUser;
+
+                    // Stel de DialogResult in op true om aan te geven dat het inloggen succesvol was
+                    DialogResult = true;
+                    Close();
+                }
+                else
+                {
+                    // Toon een foutbericht als de inloggegevens onjuist zijn
+                    MessageBox.Show("Ongeldige inloggegevens. Probeer opnieuw.", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Toon een foutbericht bij andere fouten
+                MessageBox.Show($"Fout bij het inloggen: {ex.Message}", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-            // Open het registratievenster
+            // Voeg hier je logica toe voor het verwerken van het registratieverzoek
+            // Open bijvoorbeeld een nieuw venster voor registratie of navigeer naar een registratiepagina
             var registerWindow = new RegisterWindow();
             registerWindow.ShowDialog();
         }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,13 +24,14 @@ namespace Examen.Net
     {
         private readonly Data.DatabaseContext dbContext;
         private List<Models.Product> cart = new List<Models.Product>();
+        private Models.User loggedInUser;
 
         public MainWindow()
         {
             InitializeComponent();
             dbContext = new Data.DatabaseContext();
             Data.Initializer.Initialize(dbContext);
-            LoadProducts(); // Load products when the window starts
+            LoadProducts(); // Laad producten wanneer het venster wordt gestart
         }
 
         private void LoadProducts()
@@ -42,15 +43,15 @@ namespace Examen.Net
             }
             catch (Exception ex)
             {
-                ShowError("Error loading products.", ex.Message);
+                ShowError("Fout bij het laden van producten.", ex.Message);
             }
         }
 
         private void ShowError(string status, string errorMessage)
         {
             StatusLabel.Text = status;
-            ErrorStatus.Text = $"Error: {errorMessage}";
-            MessageBox.Show($"Error: {errorMessage}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            ErrorStatus.Text = $"Fout: {errorMessage}";
+            MessageBox.Show($"Fout: {errorMessage}", "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void AddToCart_Click(object sender, RoutedEventArgs e)
@@ -78,29 +79,52 @@ namespace Examen.Net
 
         private void PlaceOrder_Click(object sender, RoutedEventArgs e)
         {
-            // Add logic for placing an order
+            // Voeg logica toe voor het plaatsen van een bestelling
             if (cart.Any())
             {
-                MessageBox.Show("Order placed!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Bestelling geplaatst!", "Succes", MessageBoxButton.OK, MessageBoxImage.Information);
                 cart.Clear();
                 RefreshCartListBox();
             }
             else
             {
-                MessageBox.Show("Shopping cart is empty. Add products before placing an order.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                MessageBox.Show("Winkelwagen is leeg. Voeg producten toe voordat je een bestelling plaatst.", "Waarschuwing", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            // Add logic for logging in
+            // Voeg logica toe voor het inloggen
             var loginWindow = new LoginWindow();
             loginWindow.ShowDialog();
+
+            // Controleer of het inloggen succesvol was en update de UI
+            if (loginWindow.DialogResult.HasValue && loginWindow.DialogResult.Value)
+            {
+                loggedInUser = loginWindow.LoggedInUser;
+                UpdateUIForLoggedInUser();
+            }
+        }
+
+        private void UpdateUIForLoggedInUser()
+        {
+            // Toon de gebruikersnaam en de "Uitloggen" knop
+            UserNameLabel.Text = $"Welkom, {loggedInUser.Username}!";
+            UserNameLabel.Visibility = Visibility.Visible;
+            LogoutButton.Visibility = Visibility.Visible;
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            // Voeg logica toe voor uitloggen
+            loggedInUser = null;
+            UserNameLabel.Visibility = Visibility.Collapsed;
+            LogoutButton.Visibility = Visibility.Collapsed;
         }
 
         private void Register_Click(object sender, RoutedEventArgs e)
         {
-            // Add logic for registering
+            // Voeg logica toe voor registreren
             var registerWindow = new RegisterWindow();
             registerWindow.ShowDialog();
         }
